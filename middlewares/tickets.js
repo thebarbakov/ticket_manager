a284679fbb66f1734f90960b4b09d3d492f3189f
Yaroslav Barbakov <94574341+thebarbakov@users.noreply.github.com>
2024-05-06 23:48:38 +0300

    ticket middleware

--- middlewares/tickets.js
+++ middlewares/tickets.js
@@ -1,25 +1,34 @@
 require("dotenv").config();
 const UnauthorizedError = require("../errors/UnauthorizedError");
+const Agent = require("../models/Agent");
 const Order = require("../models/Order");
+const User = require("../models/User");
+const { JWT_SECRET } = process.env;
 
 const checkTicket = async (req, res, next) => {
   try {
-    if (!req.originalUrl.match(/\/api\/assets\/tickets\/(.*)\_.*\.pdf/)?.[1])
-      return next();
-    if (req?.user?.access?.orders === true) return next();
+    // if (!req.originalUrl.match(/\/api\/assets\/tickets\/(.*)\_.*\.pdf/)?.[1])
+    //   return next();
 
-    const order_id = req.originalUrl.match(
-      /\/api\/assets\/tickets\/(.*)\_.*\.pdf/
-    )?.[1];
+    const { ticket_manager_user, ticket_manager_agent } = req.cookies;
 
-    const order = await Order.findOne({ _id: order_id });
-    console.log(order);
-    console.log(String(order.agent_id));
-    console.log(String(req.agent?._id));
-    if (String(order.agent_id) !== String(req.agent?._id))
+    if (ticket_manager_user) {
+      const { user_id } = jwt.verify(ticket_manager_user, JWT_SECRET);
+      const user = await User.findOne({ _id: user_id });
+      if (req?.user?.access?.orders === true) return next();
+    } else if (ticket_manager_agent) {
+      const { agent_id } = jwt.verify(ticket_manager_agent, JWT_SECRET);
+      const agent = await Agent.findOne({ _id: agent_id });
+      const order_id = req.originalUrl.match(
+        /\/api\/assets\/tickets\/(.*)\_.*\.pdf/
+      )?.[1];
+      const order = await Order.findOne({ _id: order_id });
+      if (String(order.agent_id) !== String(agent?._id))
+        return next(new UnauthorizedError("Необходима авторизация"));
+      else return next();
+    } else {
       return next(new UnauthorizedError("Необходима авторизация"));
-
-    return next();
+    }
   } catch (e) {
     return next(e);
   }
