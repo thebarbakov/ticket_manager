48b84e3abe8820d00c45da7c305911b6632b85eb
Yaroslav Barbakov <94574341+thebarbakov@users.noreply.github.com>
2024-05-07 23:13:31 +0300

    cancel booked send email

--- utils/cron/cancelBookedOrder.js
+++ utils/cron/cancelBookedOrder.js
@@ -1,8 +1,28 @@
+const Agent = require("../../models/Agent");
 const Config = require("../../models/Config");
 const Order = require("../../models/Order");
+const sendOrderChangeStatus = require("../mail/sendOrderChangeStatus");
+const { SYSTEM_URL } = process.env;
 
 const cancelBookedOrder = async () => {
   const config = await Config.findOne({ key: "orders.cancel_after_booked" });
+  
+  const orders = await Order.find({
+    status: "booked",
+    created_date: {
+      $lte: new Date().setHours(new Date().getHours() - config.value),
+    },
+  });
+
+  for await (const { _doc } of orders) {
+    const agent = await Agent.findOne({ _id: _doc.agent_id });
+    await sendOrderChangeStatus({
+      agent,
+      order: _doc,
+      link: `${SYSTEM_URL}/profile/orders/${_doc._id}`,
+    });
+  }
+  
   await Order.updateMany(
     {
       status: "booked",
