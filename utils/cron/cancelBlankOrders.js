59fc09c7875db6cef1e2ea62c4aba894c80abdf3
Yaroslav Barbakov <94574341+thebarbakov@users.noreply.github.com>
2024-05-08 17:39:51 +0300

    try catch cron

--- utils/cron/cancelBlankOrders.js
+++ utils/cron/cancelBlankOrders.js
@@ -2,18 +2,24 @@ const Config = require("../../models/Config");
 const Order = require("../../models/Order");
 
 const cancelBlankOrders = async () => {
-  const config = await Config.findOne({ key: "orders.cancel_after_booked" });
-  await Order.updateMany(
-    {
-      status: "blank",
-      created_date: {
-        $lte: new Date(
-          new Date().setMinutes(new Date().getMinutes() - Number(config.value))
-        ),
+  try {
+    const config = await Config.findOne({ key: "orders.cancel_after_booked" });
+    await Order.updateMany(
+      {
+        status: "blank",
+        created_date: {
+          $lte: new Date(
+            new Date().setMinutes(
+              new Date().getMinutes() - Number(config.value)
+            )
+          ),
+        },
       },
-    },
-    { status: "canceled" }
-  );
+      { status: "canceled" }
+    );
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 module.exports = cancelBlankOrders;
--- utils/cron/cancelBookedOrder.js
+++ utils/cron/cancelBookedOrder.js
@@ -5,36 +5,44 @@ const sendOrderChangeStatus = require("../mail/sendOrderChangeStatus");
 const { SYSTEM_URL } = process.env;
 
 const cancelBookedOrder = async () => {
-  const config = await Config.findOne({ key: "orders.cancel_after_booked" });
+  try {
+    const config = await Config.findOne({ key: "orders.cancel_after_booked" });
 
-  const orders = await Order.find({
-    status: "booked",
-    created_date: {
-      $lte:  new Date(new Date().setHours(new Date().getHours() - Number(config.value))),
-    },
-  });
-
-  for await (const order of orders) {
-    const agent = await Agent.findOne({ _id: order.agent_id });
-    order.status = "canceled";
-    order.is_payed = false;
-
-    await sendOrderChangeStatus({
-      agent,
-      order: order,
-      link: `${SYSTEM_URL}/profile/orders/${order._id}`,
-    });
-  }
-
-  await Order.updateMany(
-    {
+    const orders = await Order.find({
       status: "booked",
       created_date: {
-        $lte:  new Date(new Date().setHours(new Date().getHours() - Number(config.value))),
-    },
-    },
-    { status: "canceled" }
-  );
+        $lte: new Date(
+          new Date().setHours(new Date().getHours() - Number(config.value))
+        ),
+      },
+    });
+
+    for await (const order of orders) {
+      const agent = await Agent.findOne({ _id: order.agent_id });
+      order.status = "canceled";
+      order.is_payed = false;
+
+      await sendOrderChangeStatus({
+        agent,
+        order: order,
+        link: `${SYSTEM_URL}/profile/orders/${order._id}`,
+      });
+    }
+
+    await Order.updateMany(
+      {
+        status: "booked",
+        created_date: {
+          $lte: new Date(
+            new Date().setHours(new Date().getHours() - Number(config.value))
+          ),
+        },
+      },
+      { status: "canceled" }
+    );
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 module.exports = cancelBookedOrder;
